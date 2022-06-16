d7c22966242040c64faec02d2dae48d32d572d07
최정필 <wjdvlf1407@gmail.com>
2022-06-16 11:50:39 +0900

    getPerson 추가

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -1,12 +1,6 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +14,6 @@ public class PhoneDao {
 	@Autowired
 	private SqlSession sqlSession;
 
-	@Autowired
-	private DataSource dataSource;
-
-	// 0. import java.sql.*;
-	private Connection conn = null;
-	private PreparedStatement pstmt = null;
-	private ResultSet rs = null;
-
 	public List<PersonVo> personSelect() {
 
 		List<PersonVo> personList = sqlSession.selectList("phonebook.selectList");
@@ -40,98 +26,26 @@ public class PhoneDao {
 	public int personInsert(PersonVo personVo) {
 		System.out.println("PhoneDao > personInsert()");
 		return sqlSession.insert("phonebook.personInsert", personVo);
-		
+
 	}
-	
+
 	// 사람 삭제
 	public int personDelete(int personId) {
 		System.out.println("PhoneDao > personDelete()");
-		
+
 		int count = sqlSession.delete("phonebook.personDelete", personId);
 		return count;
 	}
-	
+
 	// 사람 수정
 	public int personUpdate(PersonVo personVo) {
-		int count = sqlSession.update("phonebook.personUpdate",personVo);
+		int count = sqlSession.update("phonebook.personUpdate", personVo);
 		return count;
-	}
-
-
-
-	private void getConnection() {
-		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			// Class.forName(driver);
-
-			// 2. Connection 얻어오기
-			// conn = DriverManager.getConnection(url, id, pw);
-			// System.out.println("접속성공");
-			conn = dataSource.getConnection();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-	}
-
-	private void close() {
-		// 5. 자원정리
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
 	}
 
 	// 1명 정보 가져오기
 	public PersonVo getPerson(int personId) {
-		PersonVo personVo = null;
-
-		this.getConnection();
-
-		try {
-
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " select  person_id, ";
-			query += "         name, ";
-			query += "         hp, ";
-			query += "         company ";
-			query += " from person ";
-			query += " where person_id = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, personId);
-
-			// 실행
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			while (rs.next()) {
-
-				int id = rs.getInt("person_id");
-				String name = rs.getString("name");
-				String hp = rs.getString("hp");
-				String company = rs.getString("company");
-
-				personVo = new PersonVo(id, name, hp, company);
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
+		PersonVo personVo = sqlSession.selectOne("phonebook.getPerson", personId);
 
 		return personVo;
 	}
